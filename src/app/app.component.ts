87933ddc5b7c9b95f0744d7d1652e37c459c425f
Brett Scott <brettsamscott@gmail.com>
2022-09-11 13:28:47 +0200

    add Router for dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+
   title(title: any) {
     throw new Error('Method not implemented.');
   }
- 
+
+constructor(public router: Router) { }
+  
 }
